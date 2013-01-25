6aae4c06e599a8192fc4ca51be714c39fbbf5a6a
Dennis Boldt <boldt@itm.uni-luebeck.de>
2013-01-25 18:16:32 +0100

    Code cleaning.

--- src/main/java/de/dennis_boldt/GrizzlyServer.java
+++ src/main/java/de/dennis_boldt/GrizzlyServer.java
@@ -1,21 +1,37 @@
 package de.dennis_boldt;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
-
-import de.dennis_boldt.Config;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
 
 public class GrizzlyServer {
 
+    private final String HOSTNAME = "localhost";
+    private final Integer PORT = 9999;
+    private final String JAXRS_RESOURCES = "de.dennis_boldt";
+
     public GrizzlyServer(int port) {
-        System.out.print("Starting Grizzly...");
+
+        // Create the URI
+        URI uri = UriBuilder.fromUri("http://" + HOSTNAME).port(PORT).build();
+
+        // Create the ResourceConfig
+        final Map<String, Object> init_params = new HashMap<String, Object>();
+        init_params.put("com.sun.jersey.config.property.packages", JAXRS_RESOURCES);
+        ResourceConfig resourceConfig =  new PackagesResourceConfig(init_params);
+
+        // Start the server
         try {
-            URI baseuri = Config.getBaseURI();
-            HttpServer server = GrizzlyServerFactory.createHttpServer(baseuri, Config.getResourceConfig());
-            System.out.println("Grizzly server started: " + baseuri.toString());
+            HttpServer server = GrizzlyServerFactory.createHttpServer(uri, resourceConfig);
+            System.out.println("Grizzly server started.");
             System.in.read();
             System.out.print("Stop server...");
             server.stop();
@@ -25,6 +41,7 @@ public class GrizzlyServer {
             e.printStackTrace();
         }
     }
+
 
     public static void main(String[] args) {
         new GrizzlyServer(9999);
